1a670e2524a437da6d437164d277139239197b49
David Castro <delahozdavid@live.com>
2024-08-08 01:38:31 -0600

    Actualizacion de Functions.py

--- src/functions/DialogflowFunctions.py
+++ src/functions/DialogflowFunctions.py
@@ -14,36 +14,37 @@ def handler_InicioChat(responseId: str):
     ## Get information from dialogflow to check if the responseId is presented in our database
     ## if responseId is presented then proceed to "inicio_nombre event response" 
     ## if not presented "name_notrelated"
-    connection = get_db_connection()
-    cursor = connection.cursor()
+    
     
     ## Once we have the database table create we can start fetching information from this one
     try:
-        cursor.execute("SELECT * FROM tabla_responsesId WHERE response_id = %s", (responseId,))
-        result = cursor.fetchone()
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM tabla_responsesId WHERE response_id = %s", (responseId,))
+                result = cursor.fetchone()
         
-        if result:
-            customer_name = result[0]
+                if result:
+                    customer_name = result[0]
             
-            return {
-                "followupEventInput": {
-                    "name": "inicio_nombre",
-                    "parameters": {
-                        "nombre": customer_name,
-                        "parameter-name-2": "no parameter value"
-                    },
-                    "languageCode": "en-US" 
-                }
-            }
-        else:
-            return {
-                "followupEventInput": {
-                    "name": "name_notrelated",
-                    "parameters": {
-                    },
-                    "languageCode": "en-US"
-                }
-            }
+                    return {
+                        "followupEventInput": {
+                            "name": "inicio_nombre",
+                            "parameters": {
+                                "nombre": customer_name,
+                                "parameter-name-2": "no parameter value"
+                            },
+                            "languageCode": "en-US" 
+                        }
+                    }
+                else:
+                    return {
+                        "followupEventInput": {
+                            "name": "name_notrelated",
+                            "parameters": {
+                            },
+                            "languageCode": "en-US"
+                        }
+                    }
             
     except Exception as error:
         return "En estos momentos estamos teniendo un inconveniente en nuestro bot, por favor contactar mas tarde"
